ac79666b9c2d1c9a8cf2b134f23468e384737f19
ren <vrenote1@virtualrenote1.(none)>
2016-01-23 14:13:01 -0200

    Add GET method - list all - list limit (query filter)

--- desafio/zipservice/views.py
+++ desafio/zipservice/views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from zipservice import postmon_tool as pt
+from zipservice.models import Address
 from zipservice.serializers import AddressSerializer
 
 
@@ -15,8 +16,8 @@ def health_check(request):
     webservice app health check
     TODO: improve
     """
-    return HttpResponse(status=200,
-                        content="OK")
+    return Response("OK",
+                    status=status.HTTP_200_OK)
 
 
 @api_view(['GET', 'POST'])
@@ -25,34 +26,52 @@ def handle_zipcode(request, zip_code=None):
     GET success return: 200
     POST success return: 201
     """
-
     if request.method == 'GET':
-        return HttpResponse(status=200,
-                            content="OK - GET - zip: %s" % zip_code)
+        if len(request.query_params) == 1 and request.query_params.get('limit'):
+            try:
+                limit = int(request.query_params.get('limit'))
+            except Exception:
+                return Response("Invalid query filter value",
+                                status=status.HTTP_400_BAD_REQUEST)
+
+        else:
+            return Response("Invalid query filter",
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if limit:
+            addresses = Address.objects.all()[:limit]
+        else:
+            addresses = Address.objects.all()
+
+        serializer = AddressSerializer(addresses, many=True)
+        return Response(serializer.data,
+                        status=status.HTTP_200_OK)
 
     if request.method == 'POST':
 
         zip_code = request.data.get('zip_code')
-        # TODO: zip_code = _validate_zip_code(zip_code)
         if not zip_code:
-            return HttpResponse(status=404,
-                                content="ERROR - POST - zip: %s NOT VALID ZIPCODE" % zip_code)
+            return Response("Zip code missing",
+                            status=status.HTTP_400_BAD_REQUEST)
 
         json_zipcode = pt.get_address_from_zipcode(zip_code)
 
-        serializer = AddressSerializer(data=json_zipcode)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if json_zipcode:
+            serializer = AddressSerializer(data=json_zipcode)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response("Zip code not found", status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['DELETE'])
 def delete_zipcode(request, zip_code=None):
     """
     success return: 204
-    error return: 
+    error return:
     """
 
-    return HttpResponse(status=204,
-                        content="OK - DELETE - zip: %s" % zip_code)
+    return Response("OK - DELETE - zip: %s" % zip_code,
+                    status=204)
